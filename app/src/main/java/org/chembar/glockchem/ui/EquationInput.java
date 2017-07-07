f1c4161f8d9f5567bbb2108e0db681a2e03a3e38
DuckSoft <ducksoft@foxmail.com>
2017-07-07 15:34:45 +0800

    EquationInput: 移动至包glockchem.ui

--- app/src/main/java/org/chembar/glockchem/ui/EquationInput.java
+++ app/src/main/java/org/chembar/glockchem/ui/EquationInput.java
@@ -1,4 +1,4 @@
-package org.chembar.glockchem;
+package org.chembar.glockchem.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.chembar.glockchem.R;
 
 public class EquationInput extends AppCompatActivity {
 
