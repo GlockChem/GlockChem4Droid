cbeac02ce866985fde0de81301170bbc212b990f
DuckSoft <ducksoft@foxmail.com>
2017-07-10 01:20:38 +0800

    CalculationOutput: 增加计算结果输出界面
    Close #8

---
+++ app/src/main/java/org/chembar/glockchem/ui/CalculationOutput.java
@@ -0,0 +1,16 @@
+package org.chembar.glockchem.ui;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import org.chembar.glockchem.R;
+
+public class CalculationOutput extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculation_output);
+    }
+
+}
