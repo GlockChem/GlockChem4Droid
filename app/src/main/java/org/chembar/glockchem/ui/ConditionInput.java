cea78e0491a1c697fb01790b567fd8e199e55de8
DuckSoft <ducksoft@foxmail.com>
2017-07-09 22:01:48 +0800

    ConditionInput: 一次飞跃
     - 基本完成方程式条件输入界面 (Close #3)
     - 解决Spinner绑定数据时崩溃退出的问题 (Close #6)
     - 实现配平后方程式的复制 (Close #7)

--- app/src/main/java/org/chembar/glockchem/ui/ConditionInput.java
+++ app/src/main/java/org/chembar/glockchem/ui/ConditionInput.java
@@ -1,45 +1,88 @@
 package org.chembar.glockchem.ui;
 
+import android.text.ClipboardManager;
 import android.content.Intent;
-import android.database.DataSetObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.chembar.glockchem.R;
 import org.chembar.glockchem.core.Equation;
+import org.chembar.glockchem.core.Formula;
+import org.chembar.glockchem.core.Pair;
+
+import java.util.ArrayList;
 
 public class ConditionInput extends AppCompatActivity {
-    Equation equInner;
-
+    private Equation equInner;
+    private int nReactants = 0; // 用于反应物的总数
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_condition_input);
 
         // 获取传递过来的Equation对象
-        Intent intent = getIntent();
-        this.equInner = (Equation) intent.getSerializableExtra("equation");
-
+        {
+            Intent intent = getIntent();
+            this.equInner = (Equation) intent.getSerializableExtra("equation");
+        }
         // 显示方程式给用户
-        TextView textEquation = (TextView) findViewById(R.id.textEquation);
-        textEquation.setText(getString(R.string.equation_hint).replace("[equation]", this.equInner.toString()));
+        {
+            TextView textEquation = (TextView) findViewById(R.id.textEquation);
+            textEquation.setText(getString(R.string.equation_hint).replace("[equation]", this.equInner.toString()));
+        }
 
         // 显示可用的条件种类
-        Spinner spinnerConditionType = (Spinner) findViewById(R.id.spinnerConditionType);
-        String[] stringsConditionType = {
-                getString(R.string.condition_type_mass),    // 质量
-                getString(R.string.condition_type_mole)     // 物质的量
-        };
-        ArrayAdapter<String> arrayAdapterConditionType = new ArrayAdapter<String>(this, R.layout.activity_condition_input, stringsConditionType);
-        // FIXME: 绑定Adapter时自动退出
-        spinnerConditionType.setAdapter(arrayAdapterConditionType);
+        {
+            Spinner spinnerConditionType = (Spinner) findViewById(R.id.spinnerConditionType);
+            String[] stringsConditionType = {
+                    getString(R.string.condition_type_mass),    // 质量
+                    getString(R.string.condition_type_mole)     // 物质的量
+            };
+            ArrayAdapter<String> arrayAdapterConditionType = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, stringsConditionType);
+            spinnerConditionType.setAdapter(arrayAdapterConditionType);
+            spinnerConditionType.setSelection(0);
+        }
 
-        // TODO: Spinner显示反应物/生成物
+        // 生成并显示反应物/生成物列表
+        {
+            // 生成反应物/生成物列表
+            ArrayList<String> arraySubstance = new ArrayList<>();
+
+            for (Pair<Formula, Integer> pair : this.equInner.reactant) {
+                arraySubstance.add(pair.getL().getRawString());
+                nReactants++;
+            }
+            for (Pair<Formula, Integer> pair : this.equInner.product) {
+                arraySubstance.add(pair.getL().getRawString());
+            }
+
+            // 显示反应物/生成物列表
+            ArrayAdapter<String> arrayAdapterSubstance = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, arraySubstance);
+            Spinner spinnerSubstance = (Spinner) findViewById(R.id.spinnerSubstance);
+            spinnerSubstance.setAdapter(arrayAdapterSubstance);
+        }
+
+        // 复制方程式按钮
+        {
+            Button buttonCopyEquation = (Button)findViewById(R.id.buttonCopyEquation);
+            buttonCopyEquation.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // 使用低版本剪贴板API以保持兼容性
+                    ClipboardManager mClipboardManager = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+                    mClipboardManager.setText(equInner.toString());
+                    Toast.makeText(ConditionInput.this, getString(R.string.equation_copied), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        // TODO: 继续按钮
+        // TODO: 返回按钮
     }
 }
