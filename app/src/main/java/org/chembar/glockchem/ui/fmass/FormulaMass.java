0588401580b850c7a229610b9d7f970ef4f73061
DuckSoft <ducksoft@foxmail.com>
2017-07-20 23:19:21 +0800

    fmass: 开坑单独化学式式量快速查询功能 (#12)

---
+++ app/src/main/java/org/chembar/glockchem/ui/fmass/FormulaMass.java
@@ -0,0 +1,15 @@
+package org.chembar.glockchem.ui.fmass;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.chembar.glockchem.R;
+
+public class FormulaMass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formula_mass);
+    }
+}
