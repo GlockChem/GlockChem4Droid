a5aa736fc785e093d6ebd1372700fc0673434eb7
DuckSoft <ducksoft@foxmail.com>
2017-07-20 23:34:07 +0800

    solution: 开坑溶液配制计算功能 (#11)

---
+++ app/src/main/java/org/chembar/glockchem/ui/solution/SolutionMenu.java
@@ -0,0 +1,15 @@
+package org.chembar.glockchem.ui.solution;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.chembar.glockchem.R;
+
+public class SolutionMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solution_menu);
+    }
+}
