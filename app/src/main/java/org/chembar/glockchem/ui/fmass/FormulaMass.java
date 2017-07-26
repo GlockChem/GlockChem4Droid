d556403cd14cc9142b3417d5ec2d7ed693936047
DuckSoft <ducksoft@foxmail.com>
2017-07-26 22:24:25 +0800

    FormulaMass: 限制用户输入化学式的格式以防止非法字符
     - Fixed #25

--- app/src/main/java/org/chembar/glockchem/ui/fmass/FormulaMass.java
+++ app/src/main/java/org/chembar/glockchem/ui/fmass/FormulaMass.java
@@ -3,6 +3,8 @@ package org.chembar.glockchem.ui.fmass;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.ClipboardManager;
+import android.text.InputFilter;
+import android.text.Spanned;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,6 +15,9 @@ import org.chembar.glockchem.core.AdvNum;
 import org.chembar.glockchem.core.Formula;
 import org.chembar.glockchem.core.RMDatabase;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FormulaMass extends AppCompatActivity {
     // 分子量数据库
     RMDatabase rmDatabase = new RMDatabase();
@@ -21,6 +26,20 @@ public class FormulaMass extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formula_mass);
+
+        // 限制输入格式
+        ((EditText) findViewById(R.id.editFormulaText)).setFilters(new InputFilter[]{
+                new InputFilter() {
+                    // NOTE: 将来Formula更新时此处也需要更新
+                    Pattern p = Pattern.compile("[^a-zA-Z0-9()*]");
+
+                    @Override
+                    public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+                        Matcher m = p.matcher(source);
+                        return m.replaceAll("");
+                    }
+                }
+        });
 
         // 返回按钮
         findViewById(R.id.buttonReturnToMenu).setOnClickListener(new View.OnClickListener() {
@@ -56,21 +75,24 @@ public class FormulaMass extends AppCompatActivity {
                 if (strInput.isEmpty()) {
                     // 为空则输出警告信息
                     Toast.makeText(FormulaMass.this, getString(R.string.err_empty_input), Toast.LENGTH_SHORT).show();
-                    return;
                 } else {    // 否则
                     try {   // 尝试分析化学式
                         // 建立对象
                         Formula formula = new Formula(strInput);
                         // 查询分子量
                         AdvNum molarMass = rmDatabase.queryMolarMass(formula);
+
                         // TODO: 数据显示
+                        // NOTE: This is a makeshift
+                        {
+                            textView.append(strInput);
+                            textView.append("---");
+                            textView.append(molarMass.toString());
+                        }
                         Toast.makeText(FormulaMass.this, "Coming soon", Toast.LENGTH_SHORT).show();
                     } catch (Exception e) {
                         Toast.makeText(FormulaMass.this, getString(R.string.formula_parse_failed), Toast.LENGTH_SHORT).show();
-                        return;
                     }
-
-
                 }
             }
         });
